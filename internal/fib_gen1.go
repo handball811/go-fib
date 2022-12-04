dd68825abd6cf63c14770305c6b7ab8d21e644db
handball811 <handball811@gmail.com>
2022-12-04 16:51:35 +0900

    FibGen2の作成

--- internal/fib_gen1.go
+++ internal/fib_gen1.go
@@ -2,14 +2,21 @@
 
 package internal
 
-func fib(n int) (int, error) {
-	if n == 0 {
-		return 0, nil
-	} else if n == 1 {
-		return 1, nil
-	}
+import "errors"
 
-	n1, _ := fib(n - 1)
-	n2, _ := fib(n - 2)
-	return n1 + n2, nil
+var ErrNegative = errors.New("negative number not allowed")
+
+func fib(n int) (int, error) {
+if n == 0 {
+return 0, nil
+} else if n < 0 {
+return 0, ErrNegative
+} else if n == 1 {
+return 1, nil
+}
+
+Copy code
+n1, _ := fib(n - 1)
+n2, _ := fib(n - 2)
+return n1 + n2, nil
 }
---
+++ internal/fib_gen2.go
@@ -0,0 +1,19 @@
+//go:build gen2
+
+package internal
+
+/// var ErrNegative = errors.New("negative value not allowed")
+
+func fib(n int) (int, error) {
+	if n < 0 {
+		return 0, ErrNegative
+	} else if n == 0 {
+		return 0, nil
+	} else if n == 1 {
+		return 1, nil
+	}
+
+	n1, _ := fib(n - 1)
+	n2, _ := fib(n - 2)
+	return n1 + n2, nil
+}
